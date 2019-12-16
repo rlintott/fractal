e7af6b68bdcf39ebeb1521e0f26d41a6ab92fac2
Ryan <rlintott@sfu.ca>
2019-12-16 13:57:52 -0800

    almost done

--- webgl.js
+++ webgl.js
@@ -3,13 +3,12 @@ glMatrix.vec2.set(globalResolution, 500.0, 500.0);
 globalZoomCenter =  glMatrix.vec2.create();
 glMatrix.vec2.set(globalZoomCenter, 0.0, 0.0); 
 mousePos =  glMatrix.vec2.create();
-globalZoomSize = 2.0
+globalZoomSize = 2.0;
 zoomin = false;
 zoomout = false;
 zoomvector = glMatrix.vec2.create();
 zoomscale = 0.025;
 thresh = 0.0;
-globalZoomSize = 2.0;
 zoomconst = 0.5;
 x_val = 0.0;
 y_val = 0.0;
@@ -31,139 +30,128 @@ function main() {
   y_val = document.getElementById("y-slider").value;
   y_val /= 100;
 
-
-
-
   // If we don't have a GL context, give up now
 
   if (!gl) {
     alert('Unable to initialize WebGL. Your browser or machine may not support it.');
     return;
   }
-  //const resolution = vec2.create();
-  //const zoomSize = 0.0
-  //const zoomCenter = vec2.create();
-  //const maxIterations = 1;
-
   // Vertex shader program
 
-    //get fragment shader
-    var fsSource = document.querySelector("#shader-fs-julia").innerHTML;
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.compileShader(fragmentShader);
-    //get vertex shader
-    var vsSource = document.querySelector("#shader-vs").innerHTML;
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.compileShader(vertexShader);
+  //get fragment shader
+  var juliasource = document.querySelector("#shader-fs-julia").innerHTML;
+  var mandelbrotsource = document.querySelector("#shader-fs-mandel").innerHTML;
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.compileShader(fragmentShader);
+  //get vertex shader
+  var vsSource = document.querySelector("#shader-vs").innerHTML;
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.compileShader(vertexShader);
 
   // Initialize a shader program; this is where all the lighting
   // for the vertices and so forth is established.
-  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+  const juliaProgram = initShaderProgram(gl, vsSource, juliasource);
+  const mandelbrotProgram = initShaderProgram(gl, vsSource, mandelbrotsource);
+
+
 
   // Collect all the info needed to use the shader program.
   // Look up which attribute our shader program is using
   // for aVertexPosition and look up uniform locations.
-  const programInfo = {
-    program: shaderProgram,
+  const juliaprogramInfo = {
+    program: juliaProgram,
     attribLocations: {
-      coords: gl.getAttribLocation(shaderProgram, 'coords'),
+      coords: gl.getAttribLocation(juliaProgram, 'coords'),
     },
     uniformLocations: {
-      resolution: gl.getUniformLocation(shaderProgram, 'u_resolution'),
-      zoomSize: gl.getUniformLocation(shaderProgram, 'u_zoomSize'),
-      zoomCenter: gl.getUniformLocation(shaderProgram, 'u_zoomCenter'),
-      maxIterations: gl.getUniformLocation(shaderProgram, 'u_maxIterations'),
-      c: gl.getUniformLocation(shaderProgram, 'u_c')
+      resolution: gl.getUniformLocation(juliaProgram, 'u_resolution'),
+      zoomSize: gl.getUniformLocation(juliaProgram, 'u_zoomSize'),
+      zoomCenter: gl.getUniformLocation(juliaProgram, 'u_zoomCenter'),
+      maxIterations: gl.getUniformLocation(juliaProgram, 'u_maxIterations'),
+      c: gl.getUniformLocation(juliaProgram, 'u_c')
     },
   };
-  gl.useProgram(shaderProgram);
 
-    gl.uniform2fv(programInfo.uniformLocations.resolution, [500.0, 500.0]);
-    gl.uniform2fv(programInfo.uniformLocations.zoomCenter, [0.0, 0.0]);
-    gl.uniform1fv(programInfo.uniformLocations.zoomSize, [2.0]);
-    gl.uniform1iv(programInfo.uniformLocations.maxIterations, [75]);
-    gl.uniform2fv(programInfo.uniformLocations.c, [x_val, y_val])
+  const mandelbrotprogramInfo = {
+    program: mandelbrotProgram,
+    attribLocations: {
+      coords: gl.getAttribLocation(mandelbrotProgram, 'coords'),
+    },
+    uniformLocations: {
+      resolution: gl.getUniformLocation(mandelbrotProgram, 'u_resolution'),
+      zoomSize: gl.getUniformLocation(mandelbrotProgram, 'u_zoomSize'),
+      zoomCenter: gl.getUniformLocation(mandelbrotProgram, 'u_zoomCenter'),
+      maxIterations: gl.getUniformLocation(mandelbrotProgram, 'u_maxIterations'),
+      c: null
+    },
+  };
+
+  programInfo = mandelbrotprogramInfo;
+
+  console.log(juliaprogramInfo.uniformLocations.resolution);
+  gl.useProgram(mandelbrotProgram);
+
+  gl.uniform2fv(programInfo.uniformLocations.resolution, [500.0, 500.0]);
+  gl.uniform2fv(programInfo.uniformLocations.zoomCenter, [0.0, 0.0]);
+  gl.uniform1fv(programInfo.uniformLocations.zoomSize, [2.0]);
+  gl.uniform1iv(programInfo.uniformLocations.maxIterations, [75]);
+  gl.uniform2fv(programInfo.uniformLocations.c, [x_val, y_val]);
+
   // Here's where we call the routine that builds all the
   // objects we'll be drawing.
-    const buffers = initBuffers(gl);
+  const buffers = initBuffers(gl);
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-  
-    {
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-        gl.vertexAttribPointer(programInfo.attribLocations.coords, 2 /*components per vertex */, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(programInfo.attribLocations.coords);
-    }
-      
-      gl.useProgram(programInfo.program);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+  gl.clearDepth(1.0);                 // Clear everything
+  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
 
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+  gl.vertexAttribPointer(programInfo.attribLocations.coords, 2 /*components per vertex */, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(programInfo.attribLocations.coords);
+    
+  gl.useProgram(programInfo.program);
 
   // Draw the scene
     function render()
     {
-        if(zoomin)
-        {
-          /* calculate move to center*/
-          //console.log(mousePos[0], mousePos[1]);
-          //console.log(globalZoomCenter[0], globalZoomCenter[1]);
-          //console.log(glMatrix.vec2.distance(globalZoomCenter, mousePos ));
-            temp = glMatrix.vec2.create();     
-            dist = glMatrix.vec2.distance(globalZoomCenter, mousePos )
-            if( dist > thresh)
-            {
-                glMatrix.vec2.set(temp, zoomvector[0], zoomvector[1]);
-                glMatrix.vec2.scale(temp, temp, zoomscale);
-                glMatrix.vec2.add(globalZoomCenter, globalZoomCenter,temp );
-                //console.log(mousePos[0], mousePos[1]);
-                //console.log(globalZoomCenter[0], globalZoomCenter[1]);
-                gl.uniform2fv(programInfo.uniformLocations.zoomCenter, globalZoomCenter);
-                console.log(thresh);
-                globalZoomSize = globalZoomSize - globalZoomSize*0.01;
-                gl.uniform1f(programInfo.uniformLocations.zoomSize, globalZoomSize);
-
-            }
-            else
-            {
+      if(zoomin)
+      {
+        /* calculate move to center*/
+          temp = glMatrix.vec2.create();     
+          dist = glMatrix.vec2.distance(globalZoomCenter, mousePos )
+          if( dist > thresh)
+          {
+              glMatrix.vec2.set(temp, zoomvector[0], zoomvector[1]);
+              glMatrix.vec2.scale(temp, temp, zoomscale);
+              glMatrix.vec2.add(globalZoomCenter, globalZoomCenter,temp );
+              //console.log(mousePos[0], mousePos[1]);
+              //console.log(globalZoomCenter[0], globalZoomCenter[1]);
+              gl.uniform2fv(programInfo.uniformLocations.zoomCenter, globalZoomCenter);
+              console.log(thresh);
               globalZoomSize = globalZoomSize - globalZoomSize*0.01;
               gl.uniform1f(programInfo.uniformLocations.zoomSize, globalZoomSize);
-            }
-            //console.log("mousedown");
-            console.log(globalZoomSize);
-            gl.uniform1i(programInfo.uniformLocations.maxIterations, 500);
 
-        }
-        else if(zoomout)
-        {
-          globalZoomSize = globalZoomSize + globalZoomSize*0.01;
-          gl.uniform1f(programInfo.uniformLocations.zoomSize, globalZoomSize);
-          gl.uniform1i(programInfo.uniformLocations.maxIterations, 500);
-        }
-        else
-        {
-          if(globalZoomSize > 0.1)
-          {
-            gl.uniform1i(programInfo.uniformLocations.maxIterations, 500);
           }
-          else if(globalZoomSize > 0.01 && globalZoomSize <= 0.1)
+          else
           {
-            gl.uniform1i(programInfo.uniformLocations.maxIterations, 500);
+            globalZoomSize = globalZoomSize - globalZoomSize*0.01;
+            gl.uniform1f(programInfo.uniformLocations.zoomSize, globalZoomSize);
           }
-          else if(globalZoomSize > 0.001 && globalZoomSize <= 0.01)
-          {
-            gl.uniform1i(programInfo.uniformLocations.maxIterations, 1000);
-          }
-          else if(globalZoomSize > 0.0001 && globalZoomSize <= 0.001)
-          {
-            gl.uniform1i(programInfo.uniformLocations.maxIterations, 5000);
-          }
-          else if(globalZoomSize <= 0.0001)
-          {
-            gl.uniform1i(programInfo.uniformLocations.maxIterations, 10000);
-          }
-        }
+          console.log(globalZoomSize);
+          //gl.uniform1i(programInfo.uniformLocations.maxIterations, 500);
+
+      }
+      else if(zoomout)
+      {
+        globalZoomSize = globalZoomSize + globalZoomSize*0.01;
+        gl.uniform1f(programInfo.uniformLocations.zoomSize, globalZoomSize);
+      }
+      else
+      {
+
+      }
+      console.log("sup");
 
     //Enter C value.
     document.getElementById("x-slider").oninput = function() {
@@ -180,6 +168,12 @@ function main() {
       gl.uniform2fv(programInfo.uniformLocations.c, [x_val, y_val]);
       //console.log(y_val);
     };
+    document.getElementById("max-iterations").oninput = function() {
+      maxIterations = document.getElementById("max-iterations").value;
+      document.getElementById("max-it").innerHTML = maxIterations;
+      gl.uniform1i(programInfo.uniformLocations.maxIterations, [maxIterations]);
+      console.log(maxIterations);
+    };
     document.getElementById("real").oninput = function() {
       x_val = document.getElementById("real").value;
       gl.uniform2fv(programInfo.uniformLocations.c, [x_val, y_val]);
@@ -190,22 +184,47 @@ function main() {
       gl.uniform2fv(programInfo.uniformLocations.c, [x_val, y_val]);
       console.log("helloy");
     };
-
-
-
-
-      drawScene(gl, programInfo, buffers);
     
-      requestAnimationFrame(render);
-
-    }
-    
+    document.getElementById("radio-one").oninput = function() {
+      programInfo = mandelbrotprogramInfo;
+      gl.useProgram(programInfo.program);
+      glMatrix.vec2.set(globalResolution, 500.0, 500.0); 
+      glMatrix.vec2.set(globalZoomCenter, 0.0, 0.0); 
+      globalZoomSize = 2.0;
+      zoomscale = 0.025;
+      thresh = 0.0;
+      zoomconst = 0.5;
+      x_val = 0.0;
+      y_val = 0.0;
+      gl.uniform2fv(programInfo.uniformLocations.resolution, [500.0, 500.0]);
+      gl.uniform2fv(programInfo.uniformLocations.zoomCenter, [0.0, 0.0]);
+      gl.uniform1fv(programInfo.uniformLocations.zoomSize, [2.0]);
+      gl.uniform1iv(programInfo.uniformLocations.maxIterations, [75]);
+      };
+    document.getElementById("radio-two").oninput = function() {
+      programInfo = juliaprogramInfo;
+      gl.useProgram(programInfo.program);
+      glMatrix.vec2.set(globalResolution, 500.0, 500.0); 
+      glMatrix.vec2.set(globalZoomCenter, 0.0, 0.0); 
+      globalZoomSize = 2.0;
+      zoomscale = 0.025;
+      thresh = 0.0;
+      zoomconst = 0.5;
+      x_val = 0.0;
+      y_val = 0.0;
+      gl.uniform2fv(programInfo.uniformLocations.resolution, [500.0, 500.0]);
+      gl.uniform2fv(programInfo.uniformLocations.zoomCenter, [0.0, 0.0]);
+      gl.uniform1fv(programInfo.uniformLocations.zoomSize, [2.0]);
+      gl.uniform1iv(programInfo.uniformLocations.maxIterations, [75]);
+      gl.uniform2fv(programInfo.uniformLocations.c, [x_val, y_val]); 
+    };
+    drawScene(gl, programInfo, buffers);
     requestAnimationFrame(render);
 
-
-
-
-}
+    }
+    requestAnimationFrame(render);
+  
+  }
 
 //
 // initBuffers
@@ -314,6 +333,7 @@ function loadShader(gl, type, source) {
 
 function mousedown(event)
 {
+  console.log("hi");
   if(event.which == 1)
   {
     var x = event.offsetX;
@@ -325,7 +345,6 @@ function mousedown(event)
 
     mousePosToComplexPlane(mousePos, globalResolution);
   }
-
   else if(event.which == 3)
   {
     zoomout = true;
@@ -360,4 +379,6 @@ function mousePosToComplexPlane(mousepos, resolution)
     thresh = glMatrix.vec2.distance(mousepos, globalZoomCenter)* 0.1;
 
 }
+
+
 
